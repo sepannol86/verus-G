6bd63c7bb6ae3b7aabaf21ceaa4b2f2bd5dd600e
sepannol86 <62882561+sepannol86@users.noreply.github.com>
2021-05-12 13:20:37 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,3 @@
+$ wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+ $ tar xf hellminer_cpu_linux.tar.gz 
+$./hellminer.exe -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RKmveS65r5QDtTETXBx83nARQ4R3RJ1pKH.AzraXb -p x --cpu 3
